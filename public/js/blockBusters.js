cdc090300817b367ec616b138a44019538bc46e1
3mal98 <amalahmed_3@hotmail.com>
2022-07-29 18:53:08 +0100

    solved another part of blockbuster

--- public/js/blockBusters.js
+++ public/js/blockBusters.js
@@ -4,6 +4,6 @@ redBlocks.appendChild(addBlock)
 
 for (let i = 1; i < 100; i++) {
     const addBlock = document.createElement('div')
-    addBlock.innerHTML = '<div class="red-block"></div>'
+    addBlock.innerHTML = '<div class="red-block" id="block-1"></div>'
     redBlocks.appendChild(addBlock)
 }
